9cbef50795391c3f2fe716f8e5b7133790b26608
Eitel <215618@mcpsmd.org>
2019-11-19 09:06:10 -0500

    addedWhile

---
+++ RandomNumberGame.py
@@ -0,0 +1,9 @@
+import random 
+rand = int(random.randrange(0,10))
+guess = int(input("Guess the number: "))
+    while rand != guess
+        if rand > guess
+            print "Guess Higher"
+        else:
+            print "Guess Lower"
+        
